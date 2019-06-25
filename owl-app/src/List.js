0140a97552c1c773d5fd44b74b84880a2a0d77de
David Omstead <davidomstead@hotmail.com>
2019-06-25 12:32:24 -0700

    Started

---
+++ owl-app/src/List.js
@@ -0,0 +1,71 @@
+import React from 'react'
+
+
+//Participants List
+
+//This file contains a ist of people. There will be ID, name, and avatar properties. 
+
+//We will then render it to the screen.
+
+
+//Function that gets list name and renders it to the screen
+
+export default function PeopleList(){
+    const participants = [
+    {
+        id: 1,
+        name: 'Koren Templeton',
+        avatar:
+            'https://robohash.org/itaquevoluptatumrerum.jpg?size=200x200&set=set1',
+        inSession: true,
+        onStage: true
+    },
+    {
+        id: 2,
+        name: 'Caty Flucker',
+        avatar:
+            'https://robohash.org/doloredolorescupiditate.jpg?size=200x200&set=set1',
+        inSession: true,
+        onStage: false
+    },
+    {
+        id: 3,
+        name: 'Axe Kubicka',
+        avatar: 'https://robohash.org/utlaborumfugit.jpg?size=200x200&set=set1',
+        inSession: false,
+        onStage: false
+    },
+    {
+        id: 4,
+        name: 'Frank Runciman',
+        avatar:
+            'https://robohash.org/etexercitationemassumenda.jpg?size=200x200&set=set1',
+        inSession: true,
+        onStage: false
+    },
+    {
+        id: 5,
+        name: 'Ashla Attwool',
+        avatar:
+            'https://robohash.org/iustodoloremqueinventore.jpg?size=200x200&set=set1',
+        inSession: true,
+        onStage: true
+    }
+]
+
+console.log(participants);
+console.log('Line 56 Ran!')
+
+const PeopleList = participants.map((participant) => (
+    <WeirdPeople key=  {participant.id} name= {participant.name}/> 
+));
+
+console.log(PeopleList)
+
+return(
+    <div className = "ListofPeople">
+        {PeopleList}
+    </div>
+)
+
+};
